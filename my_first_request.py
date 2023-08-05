9ab91a8c982d6245dba15be4faffe6a5dc46f3e9
Rachael <rhg108@gmail.com>
2023-08-05 16:27:42 -0600

    first request to open ai completion

---
+++ my_first_request.py
@@ -0,0 +1,9 @@
+import openai
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+openai.api_key = config['OPENAI_API_KEY']
+x=openai.Completion.create(
+  model="text-davinci-003",
+  prompt="The dog says "
+)
+print(x)
